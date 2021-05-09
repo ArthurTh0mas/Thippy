d5400aa95f860e19feb4fff1e8a889055ad7569c
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2021-05-10 06:29:11 +0800

    Fix `spec_version` valentin

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -199,7 +199,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("thippy"),
 	impl_name: create_runtime_str!("thippy"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 13,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
